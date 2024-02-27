3ff24a6cabebede8684ea90b0c850ea53d468d9d
Danish Ahmed <danish.ahmed@leasepoint.aero>
2024-02-27 07:08:18 +0000

    almost finished

--- pods/main.tf
+++ pods/main.tf
@@ -1,17 +1,17 @@
 
 
 
-# resource "kubernetes_namespace" "ns" {
-#   metadata {
-#     name = var.namespace
-#   }
-# }
+resource "kubernetes_namespace" "ns" {
+  metadata {
+    name = var.namespace
+  }
+}
 
 
-# module "nginx-controller" {
-#   source = "../modules/ingress/"
+module "nginx-controller" {
+  source = "../modules/ingress/"
 
-# }
+}
 
 
 module "app" {
@@ -63,37 +63,37 @@ module "masterdataapi" {
 }
 
 
-# module "iamapi" {
-#   source           = "../modules/api/iam"
-#   name             = "lp-iamapi"
-#   namespace        = var.namespace
-#   environment      = var.environment
-#   registry_server  = var.registry_server
-#   iamapi_image     = var.iamapi_image
-#   api_ingress_host = var.api_ingress_host
-#   ingress_secret   = var.ingress_secret
-#   depends_on = [
-#      module.tntcatlog,
-#     module.nginx-controller
+module "iamapi" {
+  source           = "../modules/api/iam"
+  name             = "lp-iamapi"
+  namespace        = var.namespace
+  environment      = var.environment
+  registry_server  = var.registry_server
+  iamapi_image     = var.iamapi_image
+  api_ingress_host = var.api_ingress_host
+  ingress_secret   = var.ingress_secret
+  depends_on = [
+    #  module.tntcatlog,
+    # module.nginx-controller
 
-#   ]
-# }
+  ]
+}
 
 
-# module "gridsapi" {
-#   source           = "../modules/api/grids"
-#   name             = "lp-gridsapi"
-#   namespace        = var.namespace
-#   environment      = var.environment
-#   registry_server  = var.registry_server
-#   gridsapi_image   = var.gridsapi_image
-#   api_ingress_host = var.api_ingress_host
-#   ingress_secret   = var.ingress_secret
-#   depends_on = [
-#   #  module.tntcatlog,
-#   #   module.nginx-controller
-#   ]
-# }
+module "gridsapi" {
+  source           = "../modules/api/grids"
+  name             = "lp-gridsapi"
+  namespace        = var.namespace
+  environment      = var.environment
+  registry_server  = var.registry_server
+  gridsapi_image   = var.gridsapi_image
+  api_ingress_host = var.api_ingress_host
+  ingress_secret   = var.ingress_secret
+  depends_on = [
+  #  module.tntcatlog,
+  #   module.nginx-controller
+  ]
+}
 
 
 module "logapi" {
@@ -112,24 +112,24 @@ module "logapi" {
 }
 
 
-# module "tntcatlog" {
-#   source                    = "../modules/api/tntcatlog"
-#   name                      = "lp-tntcatlogapi"
-#   namespace                 = var.namespace
-#   environment               = var.environment
-#   registry_server           = var.registry_server
-#   tntcatlogapi_image        = var.tntcatlogapi_image
-#   user_assigned_identity_id = var.user_assigned_identity_id
-#   use_vm_managed_identity   = var.use_vm_managed_identity
-#   use_pod_identity          = var.use_pod_identity
-#   azure_resource_group      = var.azure_resource_group
-#   azure_subscription_id     = var.azure_subscription_id
-#   azure_tenant_id           = var.azure_tenant_id
-#   azure_keyvault_name       = var.azure_keyvault_name
-#   depends_on = [
+module "tntcatlog" {
+  source                    = "../modules/api/tntcatlog"
+  name                      = "lp-tntcatlogapi"
+  namespace                 = var.namespace
+  environment               = var.environment
+  registry_server           = var.registry_server
+  tntcatlogapi_image        = var.tntcatlogapi_image
+  user_assigned_identity_id = var.user_assigned_identity_id
+  use_vm_managed_identity   = var.use_vm_managed_identity
+  use_pod_identity          = var.use_pod_identity
+  azure_resource_group      = var.azure_resource_group
+  azure_subscription_id     = var.azure_subscription_id
+  azure_tenant_id           = var.azure_tenant_id
+  azure_keyvault_name       = var.azure_keyvault_name
+  depends_on = [
      
-#     module.nginx-controller
-#   ]
+    # module.nginx-controller
+  ]
 
-# }
+}
 
